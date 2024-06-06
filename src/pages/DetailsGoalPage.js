766b632fe25983274de596f7ac71308ab40ac094
Rainie <wsc22@ic.ac.uk>
2024-06-06 21:40:32 +0800

    allow change on details when picking goals from suggestion

--- src/pages/DetailsGoalPage.js
+++ src/pages/DetailsGoalPage.js
@@ -3,7 +3,8 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { collection, query, where, getDocs, addDoc, updateDoc, increment, doc, arrayRemove, arrayUnion } from 'firebase/firestore';
 import { firestore } from '../firebase';
 import Menu from '../components/Menu.js';
-import './DetailsGoalPage.css'; // Import the CSS file
+import './DetailsGoalPage.css';
+import Modal from '../components/Modal';
 
 const DetailsGoalPage = () => {
   const { goalTitle } = useParams(); // Get the goalTitle from the URL params
@@ -13,6 +14,9 @@ const DetailsGoalPage = () => {
   const [featuredStories, setFeaturedStories] = useState([]);
   const [userUsefulStories, setUserUsefulStories] = useState({});
   const [showAllStories, setShowAllStories] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [newGoalCosts, setNewGoalCosts] = useState('');
+  const [newGoalCategory, setNewGoalCategory] = useState('');
 
   const navigate = useNavigate();
   const username = "Wendy237"; // Replace with the actual username
@@ -46,7 +50,7 @@ const DetailsGoalPage = () => {
             acc[story.id] = story.useful.includes(username);
             return acc;
           }, {});
-          
+
           setFeaturedStories(storiesData);
           setUserUsefulStories(userUsefulMap);
 
@@ -79,12 +83,12 @@ const DetailsGoalPage = () => {
     try {
       const goalDocQuery = query(collection(firestore, "goals"), where("titlelc", "==", formatGoalTitle(goalTitle)));
       const goalDocSnapshot = await getDocs(goalDocQuery);
-  
+
       if (!goalDocSnapshot.empty) {
         const goalDocRef = goalDocSnapshot.docs[0].ref;
         const storyRef = doc(firestore, `goals/${goalDocRef.id}/featured_s&t`, storyId);
         const currentUsefulState = userUsefulStories[storyId];
-  
+
         if (currentUsefulState) {
           // Remove user from useful array
           console.log("Removing user from useful array:", username);
@@ -98,18 +102,18 @@ const DetailsGoalPage = () => {
             useful: arrayUnion(username)
           });
         }
-  
+
         // Update local state
         setUserUsefulStories(prevState => ({
           ...prevState,
           [storyId]: !currentUsefulState
         }));
-  
+
         // Update useful count locally
         setFeaturedStories(prevStories => prevStories.map(story => {
           if (story.id === storyId) {
-            const newUsefulArray = currentUsefulState ? 
-              story.useful.filter(user => user !== username) : 
+            const newUsefulArray = currentUsefulState ?
+              story.useful.filter(user => user !== username) :
               [...story.useful, username];
             return { ...story, useful: newUsefulArray };
           }
@@ -121,10 +125,54 @@ const DetailsGoalPage = () => {
     } catch (error) {
       console.error('Error updating useful status:', error);
     }
-  };  
+  };
 
 
-  const handleSetGoal = async () => {
+  /* const handleSetGoal = async () => {
+     if (goalData) {
+       try {
+         const goalQuery = query(collection(firestore, "goals"), where("titlelc", "==", formatGoalTitle(goalTitle)));
+         const goalSnapshot = await getDocs(goalQuery);
+ 
+         if (!goalSnapshot.empty) {
+           const goalDocRef = goalSnapshot.docs[0].ref;
+ 
+           await updateDoc(goalDocRef, {
+             savers: increment(1),
+           });
+         }
+         const userQuery = query(collection(firestore, 'users'), where('Username', '==', username));
+         const userSnapshot = await getDocs(userQuery);
+ 
+         if (!userSnapshot.empty) {
+           const userId = userSnapshot.docs[0].id;
+           const goalsCollectionRef = collection(firestore, `users/${userId}/current_goals`);
+           const newGoalData = {
+             title: goalData.title,
+             progress: 0,
+             costs: averageCosts,
+             category: goalData.category[0]
+           };
+           await addDoc(goalsCollectionRef, newGoalData);
+           setIsGoalSet(true);
+           navigate('/home');
+         } else {
+           console.log('User not found');
+         }
+       } catch (error) {
+         console.error('Error setting goal: ', error);
+       }
+     }
+   };*/
+
+  const handleSetGoal = () => {
+    setShowModal(true);
+    setNewGoalCosts('');
+    setNewGoalCategory('');
+    document.body.style.overflow = 'hidden';
+  };
+
+  const handleAddNewGoal = async () => {
     if (goalData) {
       try {
         const goalQuery = query(collection(firestore, "goals"), where("titlelc", "==", formatGoalTitle(goalTitle)));
@@ -146,8 +194,8 @@ const DetailsGoalPage = () => {
           const newGoalData = {
             title: goalData.title,
             progress: 0,
-            costs: averageCosts,
-            category: goalData.category[0]
+            costs: parseFloat(newGoalCosts !== '' ? newGoalCosts : averageCosts),
+            category: newGoalCategory !== '' ? newGoalCategory : category[0]
           };
           await addDoc(goalsCollectionRef, newGoalData);
           setIsGoalSet(true);
@@ -159,9 +207,15 @@ const DetailsGoalPage = () => {
         console.error('Error setting goal: ', error);
       }
     }
+    setShowModal(false);
+    document.body.style.overflow = 'auto'; 
   };
 
-  
+  const handleModalClose = () => {
+    setShowModal(false);
+    document.body.style.overflow = 'auto'; 
+  };
+
   const toggleShowAllStories = () => {
     setShowAllStories(!showAllStories);
   };
@@ -171,7 +225,7 @@ const DetailsGoalPage = () => {
     return <div>Loading...</div>;
   }
 
-  const { title, savers, achievers, url } = goalData;
+  const { title, savers, achievers, url, category } = goalData;
 
   return (
     <div className="details-container">
@@ -181,8 +235,46 @@ const DetailsGoalPage = () => {
       <p className="goal-info">The average costs is £{averageCosts}</p>
       <p className="goal-info">{savers} users saving for this goal</p>
       <p className="goal-info">{achievers} users achieved this goal</p>
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <h2 className="modal-title">Add New Goal</h2>
+            <div className="modal-input">
+              <label htmlFor="new-goal-title">Goal: {title}</label>
+            </div>
+            <div className="modal-input">
+              <label htmlFor="new-goal-costs">Costs:</label>
+              <input
+                type="number"
+                id="new-goal-costs"
+                value={newGoalCosts !== '' ? newGoalCosts : averageCosts}
+                onChange={(e) => setNewGoalCosts(e.target.value)}
+              />
+            </div>
+            <div className="modal-input">
+              <label htmlFor="new-goal-category">Category:</label>
+              <select
+                id="new-goal-category"
+                value={newGoalCategory !== '' ? newGoalCategory : category[0]}
+                onChange={(e) => setNewGoalCategory(e.target.value)}
+              >
+                <option value="">Select a category</option>
+                <option value="Tech Gadgets">Tech Gadgets</option>
+                <option value="Fashion and Accessories">Fashion and Accessories</option>
+                <option value="Travel">Travel</option>
+                <option value="Entertainment">Entertainment</option>
+                <option value="Education and Personal Development">Education and Personal Development</option>
+                <option value="Social and Lifestyle">Social and Lifestyle</option>
+              </select>
+            </div>
+            <button className="modal-button" onClick={handleAddNewGoal}>Add New Goal</button>
+            <button className="modal-close" onClick={handleModalClose}>Close</button>
+          </div>
+        </div>
+      )}
+
       {!isGoalSet && <button className="set-goal-button" onClick={handleSetGoal}>Set Goal</button>}
-     {/* <p className="goal-info">Users expected to achieve within a similar timeframe as you:</p>
+      {/* <p className="goal-info">Users expected to achieve within a similar timeframe as you:</p>
       <div className="profile-container">
         <div className="profile">
           <img src={profilePic} alt="Profile" className="profile-pic" />
@@ -192,27 +284,27 @@ const DetailsGoalPage = () => {
       </div>
   */}
       {/* Render featured stories and tips */}
-            <h2 className="section-title">Featured Stories & Tips</h2>
-      <div className="featured-stories">
+      <h2 className="section-title">Featured Stories & Tips</h2>
+      <div className={`featured-stories ${showModal ? 'grayed-out' : ''}`}>
         {featuredStories.sort((a, b) => b.useful.length - a.useful.length).slice(0, showAllStories ? undefined : 3).map((story, index) => (
           <div key={index} className="story">
-          <p className="story-author">{story.username}</p>
-          <p className="story-content">{story.content}</p>
-          <div className="story-header">
-            <p className="story-useful">{story.useful.length} users think this is helpful.</p>
-            <button
-              className={`useful-button ${userUsefulStories[story.id] ? 'active' : ''}`}
-              onClick={() => handleUsefulClick(story.id)}
-            >
-              {userUsefulStories[story.id] ? 'Liked' : 'Like'}
+            <p className="story-author">{story.username}</p>
+            <p className="story-content">{story.content}</p>
+            <div className="story-header">
+              <p className="story-useful">{story.useful.length} users think this is helpful.</p>
+              <button
+                className={`useful-button ${userUsefulStories[story.id] ? 'active' : ''}`}
+                onClick={() => handleUsefulClick(story.id)}
+              >
+                {userUsefulStories[story.id] ? 'Liked' : 'Like'}
 
-            </button>
+              </button>
+            </div>
+
           </div>
-          
-        </div>
-        
+
         ))}
-      </div>
+      </div> 
       {featuredStories.length > 3 && (
         <button onClick={toggleShowAllStories}>
           {showAllStories ? 'Collapse' : 'View More'}
