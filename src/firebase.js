bf2cde2eb615b40022150968194dd56aa3103bdd
Shaban252 <m_shab_k@hotmail.co.uk>
2024-06-13 09:33:46 +0100

    Fixed firebase initialisation.

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 import 'firebase/firestore';
 import 'firebase/storage';
@@ -17,15 +18,20 @@ import 'firebase/storage';
 };*/
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDt9p7b8cmcUI7RgSwxC_JBxpbdp3L1joU",
-    authDomain: "goal-simple.firebaseapp.com",
-    projectId: "goal-simple",
-    storageBucket: "goal-simple.appspot.com",
-    messagingSenderId: "33739735162",
+    apiKey: "AIzaSyAt0eRb_fpMx8HgvlqhHYD2WhbjS9nFZcQ",
+    authDomain: "saving-goal-1e180.firebaseapp.com",
+    projectId: "saving-goal-1e180",
+    storageBucket: "saving-goal-1e180.appspot.com",
+    messagingSenderId: "472859181501",
     appId: "1:472859181501:web:36df775371b082c7fd52d4",
-};
+    measurementId: "G-JM8Z1W6LX9"
+  };
 
 const app = initializeApp(firebaseConfig);
 
 export const firestore = getFirestore(app);
-export const storage = getStorage(app)
+const db = getFirestore(app);
+export const storage = getStorage(app);
+const auth = getAuth(app);
+
+export { db, auth };
